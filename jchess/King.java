4877055c16879d8a259c4a61d7c0e90e668886c3
aadelabk <aadel999@gmail.com>
2024-10-30 13:49:22 +0100

    crossmark

--- jchess/King.java
+++ jchess/King.java
@@ -27,22 +27,18 @@ package jchess;
  * no other escape then stay on a square "in danger" by the opponent
  * then it's a CheckedMate, and the game is over.
  *
- *       |_|_|_|_|_|_|_|_|7
-        |_|_|_|_|_|_|_|_|6
-        |_|_|_|_|_|_|_|_|5
+        |X|_|_|_|_|_|_|X|7
+        |_|X|_|_|_|_|X|_|6
+        |_|_|X|_|_|X|_|_|5
         |_|_|X|X|X|_|_|_|4
         |_|_|X|K|X|_|_|_|3
-        |_|_|X|X|X|_|_|_|2
-        |_|_|_|_|_|_|_|_|1
-        |_|_|_|_|_|_|_|_|0
+        |_|_|X|X|X|X|_|_|2
+        |_|X|_|_|_|_|X|_|1
+        |X|_|_|_|_|_|_|X|0
         0 1 2 3 4 5 6 7
  */
-<<<<<<< Updated upstream
 
 //git flow check feature branch
-=======
-//this is the fourth comment for push
->>>>>>> Stashed changes
 
 import java.awt.Image;
 import java.util.ArrayList;
